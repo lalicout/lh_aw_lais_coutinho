b9cdf141fabbe9ac7f7b0cfc24b6d2b30d834409
Lais Coutinho <laismcs89@gmail.com>
2025-01-26 23:54:09 -0300

    Add dim_vendedor model for salesperson dimension with personal, regional, and sales target information

---
+++ aw_dbt/models/marts/dim_vendedor.sql
@@ -0,0 +1,26 @@
+select
+
+    sp.sales_person_id as pk_vendedor
+    , p.full_name as nome_vendedor
+    , e.job_title as cargo
+    , sp.territory_id as fk_territorio
+    , sp.sales_quota as meta_vendas
+    , e.hire_date as data_contratacao
+
+    , case 
+        when e.current_flag = 1 then 'Ativo'
+        else 'Desligado'
+      end as status_vendedor
+
+    , e.gender as genero
+
+    , e.birth_date as data_nascimento
+    
+
+from {{ ref('stg_sales_salesperson') }} sp
+left join {{ ref('stg_humanresources_employee') }} e
+    on sp.sales_person_id = e.employee_id
+left join {{ ref('stg_person_person') }} p
+    on sp.sales_person_id = p.person_id
+where p.person_type = 'SP'
+
