effa2d7fab2aff8121e4ebe31f8cd8f008521a9f
Lais Coutinho <laismcs89@gmail.com>
2025-01-27 04:00:12 -0300

    Add fato_logistica model to capture logistics data including purchase operations, suppliers, shipping methods, and related costs

---
+++ aw_dbt/models/marts/fato_logistica.sql
@@ -0,0 +1,40 @@
+with purchase_data as (
+    
+    select
+
+        pod.product_id as fk_produto
+        , poh.vendor_id as fk_fornecedor
+        , poh.ship_method_id as fk_metodo_envio
+        , poh.order_date as fk_data
+        , pod.order_quantity
+        , pod.received_quantity
+        , pod.rejected_quantity
+        , pod.unit_price * pod.received_quantity as custo_total
+    
+    from {{ ref('stg_purchase_purchaseorderdetail') }} pod
+    inner join {{ ref('stg_purchase_purchaseorderheader') }} poh
+        on pod.purchase_order_id = poh.purchase_order_id
+),
+joined_with_supplier as (
+    
+    select
+    
+        pd.fk_produto
+        , pd.fk_fornecedor
+        , pd.fk_metodo_envio
+        , pd.fk_data
+        , dp.nome_produto
+        , vf.nome_fornecedor
+        , pd.order_quantity
+        , pd.received_quantity
+        , pd.rejected_quantity
+        , pd.custo_total
+    
+    from purchase_data pd
+    inner join {{ ref('dim_produtos') }} dp
+        on pd.fk_produto = dp.pk_produto
+    inner join {{ ref('dim_fornecedor') }} vf
+        on pd.fk_fornecedor = vf.pk_fornecedor
+)
+select *
+from joined_with_supplier
