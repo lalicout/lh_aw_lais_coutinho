1075af4f08f2c3cd93c9bd59a885801a2daaddd0
Lais Coutinho <laismcs89@gmail.com>
2025-02-02 18:03:24 -0300

    Refactor agg_vendas_vendedor_regiao model to improve query structure and enhance address information retrieval

--- aw_dbt/models/marts/agg_vendas_vendedor_regiao.sql
+++ aw_dbt/models/marts/agg_vendas_vendedor_regiao.sql
@@ -1,24 +1,52 @@
-select
+with 
+    fato_vendas as (
+        select * from {{ ref('fato_vendas') }}
+    ),
+    
+    dim_produtos as (
+        select * from {{ ref('dim_produtos') }}
+    ),
+    
+    dim_clientes as (
+        select * from {{ ref('dim_clientes') }}
+        where tipo_cliente = 'B2B'
+    ),
+    
+    join_enderecos as (
+       
+         select 
+              a.addressid as fk_endereco_faturamento
+              , a.city as cidade
+              , sp.state_province_name as estado
+              , cr.country_name as pais
 
-    fv.vendedor_id,
-    dl.nome_territorio,
-    dl.zona_geografica,
-    sum(fv.quantidade_pedida) as total_quantidade_vendida,
-    sum(fv.receita_bruta) as total_receita_bruta,
-    sum(fv.receita_liquida) as total_receita_liquida,
-    sum(fv.valor_desconto) as total_valor_desconto,
-    sum(fv.valor_total) as total_valor_total
+        from {{ ref('stg_person_address') }} a
+        left join {{ ref('stg_person_state_province') }} sp
+            on a.stateprovinceid = sp.id_state_province
+        left join {{ ref('stg_person_region_country') }} cr
+            on sp.country_region_code = cr.country_region_code
+        ),
+            
+        agg_vendas_vendedor_regiao as (
+        select
+            je.pais
+            , je.estado
+            , je.cidade
+            , dc.nome_da_loja
+            , dp.nome_produto
+            , dp.categoria_produto
+            , dp.subcategoria_produto
+            , dp.pk_produto
+            , fv.quantidade_pedida
+            , fv.preco_unitario
+            , fv.data_pedido
+            , fv.vendedor_id
+ 
+                
+        from fato_vendas fv
+        left join dim_clientes dc on fv.cliente_id = dc.pk_cliente
+        left join join_enderecos je on je.fk_endereco_faturamento = fv.cliente_id
+        left join dim_produtos dp on fv.produto_id = dp.pk_produto
+        )
 
-from {{ ref('fato_vendas') }} fv
-left join {{ ref('dim_local') }} dl
-    on fv.territorio_id = dl.fk_territorio 
-
-where fv.status_pedido = 'Concluído' /* considera somente as vendas efetivadas */
-
-group by
-    fv.vendedor_id,
-    dl.nome_territorio,
-    dl.zona_geografica
-order by
-    dl.nome_territorio,
-    fv.vendedor_id
+    select * from agg_vendas_vendedor_regiao
